1144f224e3d89315090e6972fdb2fdbd9ac9f773
chandoluManikanta <86656805+chandoluManikanta@users.noreply.github.com>
2021-07-18 22:12:29 +0530

    Add files via upload

---
+++ quiz_app/quiz-app-master/app/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Route, Router, IndexRoute, hashHistory } from 'react-router';
+import Main from 'Main';
+
+import 'style!css!applicationStyles';
+
+render(
+    <Main />, 
+    document.getElementById("react-container")
+);
